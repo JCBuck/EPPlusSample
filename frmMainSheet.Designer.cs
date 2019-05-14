99ae86d8c88e1047b83e4716a8d7301085ee633a
Justin Chen <Justin@virtualworldarcade.com>
2019-05-14 08:14:34 -0400

    Add project files.

---
+++ frmMainSheet.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace EPPlusSample
+{
+    partial class frmMainSheet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSheet = new System.Windows.Forms.DataGridView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.Open = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSheet)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvSheet
+            // 
+            this.dgvSheet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSheet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSheet.Location = new System.Drawing.Point(0, 0);
+            this.dgvSheet.Name = "dgvSheet";
+            this.dgvSheet.Size = new System.Drawing.Size(624, 407);
+            this.dgvSheet.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnClose);
+            this.splitContainer1.Panel1.Controls.Add(this.Open);
+            this.splitContainer1.Panel1.Controls.Add(this.btnSave);
+            this.splitContainer1.Panel1.Controls.Add(this.btnNew);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dgvSheet);
+            this.splitContainer1.Size = new System.Drawing.Size(624, 441);
+            this.splitContainer1.SplitterDistance = 30;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(246, 5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(165, 5);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 2;
+            this.Open.Text = "&Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(84, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(3, 5);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 0;
+            this.btnNew.Text = "&New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.BtnNew_Click);
+            // 
+            // frmMainSheet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmMainSheet";
+            this.Text = "EPPlus XLSX Editor";
+            this.Load += new System.EventHandler(this.FrmMainSheet_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSheet)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSheet;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ frmMainSheet.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+namespace EPPlusSample
+{
+    
+    public partial class frmMainSheet : Form
+    {
+        
+
+
+        public frmMainSheet()
+        {
+            //https://stackoverflow.com/a/6362414/1764583
+            //https://stackoverflow.com/a/27280598/1764583
+            // To embed a dll in a compiled exe:
+            // 1 - Change the properties of the dll in References so that Copy Local=false
+            // 2 - Add the dll file to the project as an additional file not just a reference
+            // 3 - Change the properties of the file so that Build Action=Embedded Resource
+            // 4 - Paste this code before Application.Run in the main exe
+            AppDomain.CurrentDomain.AssemblyResolve += (Object sender, ResolveEventArgs args) =>
+            {
+                String thisExe = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+                System.Reflection.AssemblyName embeddedAssembly = new System.Reflection.AssemblyName(args.Name);
+                String resourceName = thisExe + "." + embeddedAssembly.Name + ".dll";
+
+                using (var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+                {
+                    Byte[] assemblyData = new Byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return System.Reflection.Assembly.Load(assemblyData);
+                }
+            };
+
+            InitializeComponent();
+
+        }
+        ExcelPackage excelPackage;
+
+        private void BtnNew_Click(object sender, EventArgs e)
+        {
+            String[,] arr = new String[5, 5];
+
+            var columnCount = arr.GetUpperBound(1) + 1;
+            var rowCount = arr.GetUpperBound(0) + 1;
+
+
+            if (dgvSheet.DataSource == null)
+            {
+                dgvSheet.Rows.Clear();
+                dgvSheet.Columns.Clear();
+            }
+            else
+            {
+                //((DataTable)dgvSheet.DataSource).Clear();
+                dgvSheet.DataSource = null;
+            }
+
+
+            for (int i = 0; i < columnCount; i++)
+            {
+                dgvSheet.Columns.Add(i.ToString(), " ");
+            }
+            for (int i = 0; i < rowCount; i++)
+            {
+                dgvSheet.Rows.AddCopy(0);
+                for (int k = 0; k < columnCount; k++)
+                {
+                    dgvSheet.Rows[i].Cells[k].Value = arr[i, k];
+                }
+            }
+            
+            if(excelPackage != null)
+                excelPackage.Dispose();
+            excelPackage = new ExcelPackage();
+            excelPackage.Workbook.Worksheets.Add("MySheet");
+            
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            if (excelPackage == null)
+                return;
+            String path = Path.Combine(Application.StartupPath, "test.xlsx");
+            var ws = excelPackage.Workbook.Worksheets[1];
+            for(int row = 0; row < dgvSheet.RowCount; row++)
+            {
+                for(int col = 0; col < dgvSheet.ColumnCount; col++)
+                {
+                    ws.Cells[row + 1, col + 1].Value = dgvSheet.Rows[row].Cells[col].Value;
+                }
+            }
+
+            if (excelPackage.File != null)
+            {
+                excelPackage.Save();
+            }
+            else
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Open Excel Files|*.xlsx";
+
+                DialogResult dr = saveFileDialog.ShowDialog();
+
+                if (dr == DialogResult.OK)
+                {
+                    excelPackage.SaveAs(new FileInfo(saveFileDialog.FileName));
+
+                    frmMainSheet.ActiveForm.Text = saveFileDialog.FileName;
+                }
+                
+            }
+
+
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Open Excel Files|*.xlsx";
+            
+            DialogResult dr = openFileDialog.ShowDialog();
+
+            //openFileDialog.FileOk
+            if (dr==DialogResult.OK)
+            {
+                excelPackage = new ExcelPackage(new FileInfo(openFileDialog.FileName));
+
+
+                frmMainSheet.ActiveForm.Text = openFileDialog.FileName;
+                var ws = excelPackage.Workbook.Worksheets[1];
+
+                int iColCnt = ws.Dimension.End.Column;
+                int iRowCnt = ws.Dimension.End.Row;
+
+
+                if (dgvSheet.DataSource == null)
+                {
+                    dgvSheet.Rows.Clear();
+                    dgvSheet.Columns.Clear();
+                }
+                else
+                {
+                    //((DataTable)dgvSheet.DataSource).Clear();
+                    dgvSheet.DataSource = null;
+                }
+
+
+
+                //===========Method 1 Fastest=============
+
+                DataTable dt = new DataTable();
+                for (int i = 0; i < iColCnt; i++)
+                    dt.Columns.Add(i.ToString(), typeof(string));
+
+                String[] cols = new String[iColCnt];
+
+                for (int i = 0; i < iRowCnt; i++)
+                {
+                    
+                    
+                    for (int k = 0; k < iColCnt; k++)
+                        cols[k] = ws.Cells[i + 1, k + 1].GetValue<String>();
+                    dt.Rows.Add(cols);
+
+                }
+                dgvSheet.DataSource = dt;
+                //===========Method 2 Faster =================
+
+                //for (int i = 0; i < iColCnt; i++)
+                //    dgvSheet.Columns.Add(i.ToString(), " ");
+
+                //https://10tec.com/articles/why-datagridview-slow.aspx
+                //List<DataGridViewRow> rows = new List<DataGridViewRow>();
+                //for (int i = 0; i <  iRowCnt; i++)
+                //{
+                //    DataGridViewRow row = new DataGridViewRow();
+                //    row.CreateCells(dgvSheet);
+                //    for (int k = 0; k < iColCnt; k++)
+                //        row.Cells[k].Value = ws.Cells[i + 1, k + 1].Value;
+
+                //    //row.Cells[0].Value = String.Format("Text {0}", i);
+                //    //row.Cells[1].Value = i;
+                //    //row.Cells[2].Value = DateTime.Now;
+                //    rows.Add(row);
+                //}
+
+                //dgvSheet.Rows.AddRange(rows.ToArray());
+                //========Method 3 Slow ==============
+                //for (int i = 0; i < iColCnt; i++)
+                //    dgvSheet.Columns.Add(i.ToString(), " ");
+
+                //This causes it to load slower
+                //for (int i = 0; i < iRowCnt; i++)
+                //{
+                //    dgvSheet.Rows.AddCopy(0);
+                //    
+
+                //    for (int k = 0; k < iColCnt; k++)
+                //    {
+                //        dgvSheet.Rows[i].Cells[k].Value = ws.Cells[i + 1, k + 1].Value;
+                //    }
+                //}
+
+            }
+
+
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            if (excelPackage != null)
+            {
+                excelPackage.Dispose();
+                if (dgvSheet.DataSource == null)
+                {
+                    dgvSheet.Rows.Clear();
+                    dgvSheet.Columns.Clear();
+                } else
+                {
+                    //((DataTable)dgvSheet.DataSource).Clear();
+                    dgvSheet.DataSource = null;
+                }
+
+                frmMainSheet.ActiveForm.Text = "";
+            }
+            
+
+        }
+
+        private void FrmMainSheet_Load(object sender, EventArgs e)
+        {
+            //https://10tec.com/articles/why-datagridview-slow.aspx
+            //enables double buffering so it's much faster
+            if (!System.Windows.Forms.SystemInformation.TerminalServerSession)
+            {
+                Type dgvType = dgvSheet.GetType();
+                PropertyInfo pi = dgvType.GetProperty("DoubleBuffered",
+                  BindingFlags.Instance | BindingFlags.NonPublic);
+                pi.SetValue(dgvSheet, true, null);
+            }
+        }
+    }
+}
